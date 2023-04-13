260f990b73b15c366ee86d5271571e6fa22a9e97
shubham kanase <shubhamkanase92@gmail.com>
2023-04-13 12:23:26 +0530

    major updates in functions and completed the game

--- script.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('div.container button');
-const rock = document.getElementById('rock');
-const paper = document.getElementById("paper");
-const scissor = document.getElementById("scissor");
-let player;
